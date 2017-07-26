b07f5e1bdde6713e7e910b128ce7fb3d1199e303
daifei <fei.dai@dmall.com>
2017-07-26 18:41:31 +0800

    修改web基础配置

--- market-web/src/main/java/com/market/controller/VirtualCoinMarketController.java
+++ market-web/src/main/java/com/market/controller/VirtualCoinMarketController.java
@@ -1,9 +1,7 @@
 package com.market.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Administrator on 17-7-17.
@@ -13,10 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class VirtualCoinMarketController {
 
     @RequestMapping(value = "/index")
-    @ResponseBody
-    public JSONObject index() {
-
-        return null;
+    public String index() {
+        return "first_vm";
     }
 
 }
