bf8d1803dce37ea70bd780d1430d89a216049f70
fei.dai <fei.dai@dmall.com>
2017-07-27 07:29:07 +0800

    jd90-50元券

--- market-web/src/main/java/com/market/controller/VirtualCoinMarketController.java
+++ market-web/src/main/java/com/market/controller/VirtualCoinMarketController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/coin")
 public class VirtualCoinMarketController {
 
+    // https://coupon.jd.com/ilink/couponSendFront/send_index.action?key=f74f06c1ccde410e9092fcbe85bf8062&roleId=7479723&to=chongzhi.jd.com/&cpdad=1DLSUE
+
     @RequestMapping(value = "/info")
     public String index() {
         return "coin_info";
